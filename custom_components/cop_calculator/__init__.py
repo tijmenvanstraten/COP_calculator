5e672bed32114bb277909332d0199559b2c6a9b1
tijmenvanstraten <tijmenvanstraten@gmail.com>
2026-02-13 09:33:30 +0100

    Update __init__.py

--- custom_components/cop_calculator/__init__.py
+++ custom_components/cop_calculator/__init__.py
@@ -1,15 +1,19 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
-async def async_setup(hass, config):
-    """Set up via YAML (deprecated)."""
+
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-async def async_setup_entry(hass, entry):
-    """Set up COP Calculator from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    await hass.config_entries.async_forward_entry_setups(
+        entry, ["sensor"]
+    )
     return True
 
-async def async_unload_entry(hass, entry):
-    """Unload a config entry."""
-    hass.data[DOMAIN].pop(entry.entry_id, None)
-    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
