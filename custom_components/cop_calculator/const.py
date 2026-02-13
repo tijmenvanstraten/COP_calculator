69e1da875a411d9afd789b5589a646eb76ad095d
tijmenvanstraten <tijmenvanstraten@gmail.com>
2026-02-13 20:12:09 +0100

    Update const.py

--- custom_components/cop_calculator/const.py
+++ custom_components/cop_calculator/const.py
@@ -1,17 +1,25 @@
-DOMAIN = "cop_calculator"
+DOMAIN = "hitachi_yutaki_cop"
 
-CONF_LANGUAGE = "language"
-CONF_DHW_VOLUME = "dhw_volume_liters"
-CONF_HEATER_OFFSET = "heater_offset_w"
+# Sensor names
+SENSOR_INDOOR_POWER = "sensor.shelly_warmtepomp_binnenunit_active_power"
+SENSOR_OUTDOOR_POWER = "sensor.shelly_warmtepomp_buitenunit_active_power"
+SENSOR_DHW_HEATER = "binary_sensor.control_unit_dhw_heater_2"
+SENSOR_OUTLET_TEMP = "sensor.control_unit_water_outlet_temperature_2"
+SENSOR_INLET_TEMP = "sensor.control_unit_water_inlet_temperature_2"
+SENSOR_FLOW = "sensor.control_unit_water_flow_2"
+SENSOR_OPERATION_STATE = "sensor.control_unit_operation_state_2"
+SENSOR_DHW_CURRENT_TEMP = "sensor.dhw_current_temperature"
+SENSOR_DHW_TARGET_TEMP = "sensor.dhw_temperatuur_set_corrected"
 
-DEFAULT_LANGUAGE = "en"
-DEFAULT_DHW_VOLUME = 260
-DEFAULT_HEATER_OFFSET = 0
+# Operation states
+STATE_HEAT_THERMO = "operation_state_heat_thermo_on"
+STATE_HEAT_COOL = "operation_state_heat_cool_on"
+STATE_DHW = "operation_state_dhw_on"
 
-# Internal energy sensor ids
-ENERGY_ELECTRIC_HEAT = "electric_energy_heating"
-ENERGY_THERMAL_HEAT = "thermal_energy_heating"
-ENERGY_ELECTRIC_COOL = "electric_energy_cooling"
-ENERGY_THERMAL_COOL = "thermal_energy_cooling"
-ENERGY_ELECTRIC_DHW = "electric_energy_dhw"
-ENERGY_THERMAL_DHW = "thermal_energy_dhw"
+# Default values
+DEFAULT_PUMP_POWER = 59
+DEFAULT_DHW_TANK_VOLUME = 260
+
+# Attributes
+ATTR_PUMP_POWER = "pump_power"
+ATTR_DHW_TANK_VOLUME = "dhw_tank_volume"
