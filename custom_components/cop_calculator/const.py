58180a6adbbac43f8789b53b14818284d002d759
tijmenvanstraten <tijmenvanstraten@gmail.com>
2026-02-13 09:33:01 +0100

    Update const.py

--- custom_components/cop_calculator/const.py
+++ custom_components/cop_calculator/const.py
@@ -1,2 +1,17 @@
-"""Constants for the COP Calculator integration."""
 DOMAIN = "cop_calculator"
+
+CONF_LANGUAGE = "language"
+CONF_DHW_VOLUME = "dhw_volume_liters"
+CONF_HEATER_OFFSET = "heater_offset_w"
+
+DEFAULT_LANGUAGE = "en"
+DEFAULT_DHW_VOLUME = 260
+DEFAULT_HEATER_OFFSET = 0
+
+# Internal energy sensor ids
+ENERGY_ELECTRIC_HEAT = "electric_energy_heating"
+ENERGY_THERMAL_HEAT = "thermal_energy_heating"
+ENERGY_ELECTRIC_COOL = "electric_energy_cooling"
+ENERGY_THERMAL_COOL = "thermal_energy_cooling"
+ENERGY_ELECTRIC_DHW = "electric_energy_dhw"
+ENERGY_THERMAL_DHW = "thermal_energy_dhw"
