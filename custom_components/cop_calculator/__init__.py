67d3c72b7ced7bc9da38549ebb06dfc58520bce5
tijmenvanstraten <tijmenvanstraten@gmail.com>
2026-02-13 20:12:49 +0100

    Update __init__.py

--- custom_components/cop_calculator/__init__.py
+++ custom_components/cop_calculator/__init__.py
@@ -1,19 +1,23 @@
-from homeassistant.config_entries import ConfigEntry
+import logging
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from homeassistant.helpers import entity_platform
+from homeassistant.helpers.storage import Store
+from .const import DOMAIN, DEFAULT_PUMP_POWER, DEFAULT_DHW_TANK_VOLUME
 
-from .const import DOMAIN
+_LOGGER = logging.getLogger(__name__)
 
-
-async def async_setup(hass: HomeAssistant, config: dict):
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Hitachi Yutaki COP integration."""
+    hass.data.setdefault(DOMAIN, {})
     return True
 
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    await hass.config_entries.async_forward_entry_setups(
-        entry, ["sensor"]
-    )
+async def async_setup_entry(hass: HomeAssistant, entry):
+    """Set up the integration from a config entry."""
+    hass.data[DOMAIN][entry.entry_id] = {
+        "pump_power": entry.data.get(ATTR_PUMP_POWER, DEFAULT_PUMP_POWER),
+        "dhw_tank_volume": entry.data.get(ATTR_DHW_TANK_VOLUME, DEFAULT_DHW_TANK_VOLUME),
+        "store": Store(hass, version=1, key=DOMAIN),
+    }
+    await entity_platform.async_setup_platforms(hass, entry, ["sensor"])
     return True
-
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
