4de47f555a399dcc93c8ea0f0f339ab7653068d7
tijmenvanstraten <tijmenvanstraten@gmail.com>
2026-02-13 20:12:30 +0100

    Update config_flow.py

--- custom_components/cop_calculator/config_flow.py
+++ custom_components/cop_calculator/config_flow.py
@@ -1,30 +1,28 @@
 from homeassistant import config_entries
+from homeassistant.core import callback
 import voluptuous as vol
-from .const import DOMAIN
+from .const import DOMAIN, ATTR_PUMP_POWER, ATTR_DHW_TANK_VOLUME, DEFAULT_PUMP_POWER, DEFAULT_DHW_TANK_VOLUME
 
-class COPCalculatorConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class HitachiYutakiCOPConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Hitachi Yutaki COP."""
+
     VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
         if user_input is not None:
             return self.async_create_entry(
-                title="COP Calculator",
-                data=user_input,
+                title="Hitachi Yutaki COP",
+                data={
+                    ATTR_PUMP_POWER: user_input.get(ATTR_PUMP_POWER, DEFAULT_PUMP_POWER),
+                    ATTR_DHW_TANK_VOLUME: user_input.get(ATTR_DHW_TANK_VOLUME, DEFAULT_DHW_TANK_VOLUME),
+                },
             )
-
-        schema = vol.Schema({
-            vol.Optional(
-                "language",
-                default="en"
-            ): vol.In(["en", "nl"]),
-
-            vol.Optional(
-                "circulation_pump_power",
-                default=59
-            ): vol.Coerce(int),
-        })
-
         return self.async_show_form(
             step_id="user",
-            data_schema=schema,
+            data_schema=vol.Schema({
+                vol.Required(ATTR_PUMP_POWER, default=DEFAULT_PUMP_POWER): int,
+                vol.Required(ATTR_DHW_TANK_VOLUME, default=DEFAULT_DHW_TANK_VOLUME): int,
+            }),
         )
