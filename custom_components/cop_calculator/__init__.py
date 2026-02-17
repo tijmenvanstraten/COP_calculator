585534e41ab4dd217e5f10072511bac7b4fe58ed
tijmenvanstraten <tijmenvanstraten@gmail.com>
2026-02-17 15:58:42 +0100

    Update __init__.py

--- custom_components/cop_calculator/__init__.py
+++ custom_components/cop_calculator/__init__.py
@@ -3,7 +3,8 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from homeassistant.helpers import entity_platform
 from homeassistant.helpers.storage import Store
-from .const import DOMAIN, DEFAULT_PUMP_POWER, DEFAULT_DHW_TANK_VOLUME
+from homeassistant.config_entries import ConfigEntry  # <-- Voeg dit toe
+from .const import DOMAIN, ATTR_PUMP_POWER, ATTR_DHW_TANK_VOLUME, DEFAULT_PUMP_POWER, DEFAULT_DHW_TANK_VOLUME  # <-- Voeg ATTR_PUMP_POWER en ATTR_DHW_TANK_VOLUME toe
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -12,7 +13,7 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     hass.data.setdefault(DOMAIN, {})
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up the integration from a config entry."""
     hass.data[DOMAIN][entry.entry_id] = {
         "pump_power": entry.data.get(ATTR_PUMP_POWER, DEFAULT_PUMP_POWER),
